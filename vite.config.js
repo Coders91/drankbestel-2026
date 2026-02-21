f8efb9f945104dc21585cafb9c77d4fbce42f029
Rody <rody_stolwijk@hotmail.com>
2026-02-21 03:42:04 +0100

    Adjust vite-config to reflect new local.drankbestel.nl structure

--- vite.config.js
+++ vite.config.js
@@ -1,44 +1,71 @@
+import fs from 'fs';
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite';
 import laravel from 'laravel-vite-plugin'
 import { wordpressPlugin, wordpressThemeJson } from '@roots/vite-plugin';
 
-export default defineConfig((config) => ({
-  base:
-    config.command === 'build'
-    ? '/wp-content/themes/drankbestel-new/public/build/' // Production (server)
-    : '/app/themes/drankbestel-new/public/build/', // Development (local)
-  plugins: [
-    tailwindcss(),
-    laravel({
-      input: [
-        // CSS
-        'resources/css/app.css',
-        'resources/css/editor.css',
+const host = 'local.drankbestel.nl';
+const certsPath = '/etc/apache2/ssl';
 
-        // JS
-        'resources/js/app.js',
-        'resources/js/editor.js',
-      ],
-      refresh: true,
-    }),
+export default defineConfig(({ command }) => {
 
-    wordpressPlugin(),
+  const config = {
+    base:
+      command === 'build'
+      ? '/wp-content/themes/drankbestel-new/public/build/' // Production (server)
+      : '/app/themes/drankbestel-new/public/build/', // Development (local)
+    plugins: [
+      tailwindcss(),
+      laravel({
+        input: [
+          // CSS
+          'resources/css/app.css',
+          'resources/css/editor.css',
 
-    // Generate the theme.json file in the public/build/assets directory
-    // based on the Tailwind config and the theme.json file from base theme folder
-    wordpressThemeJson({
-      disableTailwindColors: false,
-      disableTailwindFonts: false,
-      disableTailwindFontSizes: false,
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@scripts': '/resources/js',
-      '@styles': '/resources/css',
-      '@fonts': '/resources/fonts',
-      '@images': '/resources/images',
+          // JS
+          'resources/js/app.js',
+          'resources/js/editor.js',
+        ],
+        refresh: true,
+      }),
+
+      wordpressPlugin(),
+
+      // Generate the theme.json file in the public/build/assets directory
+      // based on the Tailwind config and the theme.json file from base theme folder
+      wordpressThemeJson({
+        disableTailwindColors: false,
+        disableTailwindFonts: false,
+        disableTailwindFontSizes: false,
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@scripts': '/resources/js',
+        '@styles': '/resources/css',
+        '@fonts': '/resources/fonts',
+        '@images': '/resources/images',
+      },
     },
-  },
-}));
+  };
+
+  if (command === 'serve') {
+    config.server = {
+      host: '0.0.0.0',
+      port: 5173,
+      https: {
+        key: fs.readFileSync(`${certsPath}/local.drankbestel.nl-key.pem`),
+        cert: fs.readFileSync(`${certsPath}/local.drankbestel.nl.pem`),
+      },
+      hmr: {
+        host: host,
+      },
+      cors: {
+        origin: `https://${host}`,
+      },
+    };
+  }
+
+  return config;
+
+});
