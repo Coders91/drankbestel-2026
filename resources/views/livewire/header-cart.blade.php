0b0020b90ae00d69ce0a01966a140fd204afba7e
Rody <rody_stolwijk@hotmail.com>
2026-02-26 19:32:53 +0100

    Header improvements

--- resources/views/livewire/header-cart.blade.php
+++ resources/views/livewire/header-cart.blade.php
@@ -4,9 +4,9 @@
     class="relative p-2 text-gray-700 hover:text-red-600 transition-colors flex"
     title="{{ __('Winkelwagen', 'sage') }}"
   >
-    @svg('resources.images.icons.shopping-cart', 'h-7')
+    @svg('resources.images.icons.shopping-cart', 'h-6')
     @if ($this->itemCount > 0)
-      <span class="absolute -top-0.25 -right-1 bg-red-600 text-white text-xs font-bold rounded-full min-w-5 h-5 flex items-center justify-center px-1">
+      <span class="absolute -top-0.25 -right-1 bg-red-600 text-white text-xs font-bold rounded-full size-5 flex items-center justify-center px-1">
         {{ $this->itemCount > 99 ? '99+' : $this->itemCount }}
       </span>
     @endif
